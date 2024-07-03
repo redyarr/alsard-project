d6faae6342f2c5d3b45c4228d1a17a2618d0e8e8
redyarr <redyarh@gmail.com>
2024-07-03 18:31:52 +0300

    mm

--- backend/server.js
+++ backend/server.js
@@ -18,32 +18,36 @@ app.post('/items', (req, res, next) => {
         Name: req.body.Name || "lenovo monitor",
         Description: req.body.Description || "has windows 10 installed",
         Category: req.body.Category || "monitor",
-        Price: req.body.Price || 250,
-        Quantity: req.body.Quantity || 1,
-        companyId: req.body.companyId || "ALSARD-IT-0000725"
+        model: req.body.model || "lenovo",
+        tagId: req.body.tagId || "alsard-it-0000725",
+        company: req.body.company || "ALSARD",
+        subLocation: req.body.subLocation || "IT-Department",
+
     })
-    .then(item => {
-        res.status(201).json({
-            message: "Item created successfully",
-            item: item
+        .then(item => {
+            res.status(201).json({
+                message: "Item created successfully",
+                item: item
+            });
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: "Failed to create item",
+                error: error.message
+            });
         });
-    })
-    .catch(error => {
-        res.status(500).json({
-            message: "Failed to create item",
-            error: error.message
-        });
-    });
 });
 
 
 app.post('/addEmployee', (req, res, next) => {
+    console.log("entered it ")
     employees.create({
         Name: req.body.name || "nmuna ",
-        Email: req.body.email ,
+        Email: req.body.email,
         Phone: req.body.phone || "1234567890",
         Address: req.body.address || "123, Main Street, City",
-        Position: req.body.position || "employee"
+        Position: req.body.position || "employee",
+        Department: req.body.department || "IT"
     }).then(result => {
         res.status(201).json({
             message: "Employee created successfully",
@@ -66,18 +70,18 @@ app.post('/addEmployee', (req, res, next) => {
 app.get('/items', (req, res, next) => {
 
     items.findAll()
-    .then(items => {
-        res.status(200).json({
-            message: "Items fetched successfully",
-            items: items
+        .then(items => {
+            res.status(200).json({
+                message: "Items fetched successfully",
+                items: items
+            });
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: "Failed to fetch items",
+                error: error.message
+            });
         });
-    })
-    .catch(error => {
-        res.status(500).json({
-            message: "Failed to fetch items",
-            error: error.message
-        });
-    });
 
 
 })
