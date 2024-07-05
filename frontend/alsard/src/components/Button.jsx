f6a480b3871d65065dfaf0bb5fd0ef810cd1f7fb
rekarrr <rekarjamal33@gmail.com>
2024-07-05 15:32:10 +0300

    Update Button.jsx

--- frontend/alsard/src/components/Button.jsx
+++ frontend/alsard/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-        <button className='w-[100px] h-[35px] bg-blue-600 text-white rounded-md font-medium'>{props.text}</button>
+        <button className='w-[100px] h-[35px] bg-blue-600 hover:bg-blue-700 text-white rounded-md font-medium'>{props.text}</button>
   )
 }
 
