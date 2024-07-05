ff1aa112b11f05e39995a3697a38a4bd3df19f8f
rekarrr <rekarjamal33@gmail.com>
2024-07-06 02:13:23 +0300

    Update Login.jsx

--- frontend/alsard/src/components/Login.jsx
+++ frontend/alsard/src/components/Login.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
     e.preventDefault();
 
     try{
-        const response = await fetch('/login', {
+        const response = await fetch('http://localhost:3000/login', {
             method:'POST',
             headers:{
                 'Content-Type': 'application/json'
