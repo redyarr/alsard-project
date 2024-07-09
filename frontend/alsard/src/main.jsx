2f1bf24fb9333123ff829e87b528305d72be891f
redyarr <redyarh@gmail.com>
2024-07-09 10:30:50 +0300

    s

--- frontend/alsard/src/main.jsx
+++ frontend/alsard/src/main.jsx
@@ -11,3 +11,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </React.StrictMode>
   </BrowserRouter>,
 )
+
+// Authors: Rekar Jamal Najm ,Redyar Hawzhin rauf.
+// Date: 2024-7-7
+// Description: This is a simple inventory management system that allows you to add employees, items, and reserve items for employees.
+//              It also allows you to view all employees and items, and view the items that an employee is using.
+//              It also allows you to delete items and employees, and update items and employees.
+//              It also allows you to search for an employee and view all the items that he is using.
