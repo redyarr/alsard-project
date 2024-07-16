169fecce9c6fde0a66bb3773cee8a92bc7d57afb
r3k4r <rekarjamal33@gmail.com>
2024-07-16 20:42:54 +0300

    print

--- frontend/alsard/src/components/EmployeeDetail.jsx
+++ frontend/alsard/src/components/EmployeeDetail.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { FaPrint } from "react-icons/fa";
+
 
 const EmployeeDetail = () => {
   const { id } = useParams();
@@ -29,15 +31,28 @@ const EmployeeDetail = () => {
 
   return (
 
-    
-    <div>
-      <h1>{employee.Name}</h1>
-      <p>Email: {employee.Email}</p>
-      <p>Department: {employee.department}</p>
-      <p>Phone: {employee.Phone}</p>
-      <p>Employee ID: {employee.employeeId}</p>
-      <p>Position: {employee.Position}</p>
+    <>
+    <div className='flex justify-between mt-5 text-black mx-auto max-w-8xl xl:px-6 2xl:px-20'>
+          <div>
+            <h1>{employee.Name}</h1>
+            <p>Email: {employee.Email}</p>
+            <p>Department: {employee.department}</p>
+            <p>Phone: {employee.Phone}</p>
+            <p>Employee ID: {employee.employeeId}</p>
+            <p>Position: {employee.Position}</p>
+          </div>
+
+
+          <div>
+              <a href="javascript:if(window.print)window.print()">
+                <button className='w-[100px] h-[40px] bg-blue-600 hover:bg-blue-700 text-white rounded-md font-medium gap-5 p-2 flex items-center'> 
+                  Print <FaPrint />
+                </button>
+              </a>
+          </div>
     </div>
+    </>
+  
   );
 };
 
